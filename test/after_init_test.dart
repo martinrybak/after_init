33d12c63c160d1fed7c906bb9ae157d0c5a0b3d8
Martin Rybak <martin.rybak@gmail.com>
2019-06-12 15:13:48 -0400

    Added widget tests

--- test/after_init_test.dart
+++ test/after_init_test.dart
@@ -1,13 +1,132 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mockito/mockito.dart';
 import 'package:after_init/after_init.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets('initState() is called exactly once', (WidgetTester tester) async {
+    //Arrange
+    final mock = MockObject();
+    final widget = TestWidget(
+      onInitState: () => mock.bar(),
+    );
+
+    //Act
+    await tester.pumpWidget(widget);
+
+    //Assert
+    verify(mock.bar()).called(1);
   });
+
+  testWidgets('didInitState() is called exactly once', (WidgetTester tester) async {
+    //Arrange
+    final mock = MockObject();
+    final widget = TestWidget(
+      onDidInitState: () => mock.bar(),
+    );
+
+    //Act
+    await tester.pumpWidget(widget);
+
+    //Assert
+    verify(mock.bar()).called(1);
+  });
+
+  testWidgets('didChangeDependencies() is called exactly once', (WidgetTester tester) async {
+    //Arrange
+    final mock = MockObject();
+    final widget = TestWidget(
+      onDidChangeDependencies: () => mock.bar(),
+    );
+
+    //Act
+    await tester.pumpWidget(widget);
+
+    //Assert
+    verify(mock.bar()).called(1);
+  });
+
+  testWidgets('didInitState() is called exactly once even if didChangeDependencies() is called again', (WidgetTester tester) async {
+    //Arrange
+    final key = GlobalKey<_TestWidgetState>();
+    final mock = MockObject();
+    final widget = TestWidget(
+      key: key,
+      onDidInitState: () => mock.bar(),
+    );
+
+    //Act
+    await tester.pumpWidget(widget);
+    key.currentState.didChangeDependencies();
+    key.currentState.didChangeDependencies();
+
+    //Assert
+    verify(mock.bar()).called(1);
+  });
+
+  testWidgets('method order is: initState(), didInitState(), didChangeDependencies()', (WidgetTester tester) async {
+    //Arrange
+    final mock1 = MockObject();
+    final mock2 = MockObject();
+    final mock3 = MockObject();
+    final widget = TestWidget(
+      onInitState: () => mock1.bar(),
+      onDidInitState: () => mock2.bar(),
+      onDidChangeDependencies: () => mock3.bar(),
+    );
+
+    //Act
+    await tester.pumpWidget(widget);
+
+    //Assert
+    verifyInOrder([
+      mock1.bar(),
+      mock2.bar(),
+      mock3.bar(),
+    ]);
+  });
+}
+
+class Foo { void bar(){} }
+
+class MockObject extends Mock implements Foo {}
+
+class TestWidget extends StatefulWidget {
+  final Function() onInitState;
+  final Function() onDidInitState;
+  final Function() onDidChangeDependencies;
+
+  const TestWidget({
+    Key key,
+    this.onInitState,
+    this.onDidInitState,
+    this.onDidChangeDependencies,
+  }) : super(key: key);
+
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> with AfterInitMixin<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.onInitState?.call();
+  }
+
+  @override
+  void afterInit() {
+    widget.onDidInitState?.call();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.onDidChangeDependencies?.call();
+  }
 }
